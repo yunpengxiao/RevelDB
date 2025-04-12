eae292084312a0b65a458bff6a9bc16d6aa4e942
yunpengxiao <yunpeng.shaw@gmail.com>
2025-04-11 17:35:39 -0700

    fix the internal key

--- src/core/mem_table.rs
+++ src/core/mem_table.rs
@@ -49,12 +49,21 @@ impl InternalKeyValue {
             + utils::varint_length(val_size as u64)
             + val_size;
         let mut buff = BytesMut::zeroed(encoded_len as usize);
+        let mut offset: usize = 0;
         let written = utils::encode_varint_32(&mut buff, internal_key_size);
-        buff.extend_from_slice(k.as_bytes());
+        offset += written;
+
+        buff[offset..offset + key_size as usize].copy_from_slice(k.as_bytes());
+        offset += key_size as usize;
+
         let tag = (sequence << 8) | k_type as u64;
-        buff.extend_from_slice(&tag.to_be_bytes());
-        utils::encode_varint_32(&mut buff[(written + key_size as usize + 8)..], val_size);
-        buff.extend_from_slice(v.as_bytes());
+        buff[offset..offset + 8].copy_from_slice(&tag.to_be_bytes());
+        offset += 8;
+
+        let written = utils::encode_varint_32(&mut buff[offset..], val_size);
+        offset += written;
+
+        buff[offset..].copy_from_slice(v.as_bytes());
 
         Self {
             data: buff.freeze(),
@@ -63,5 +72,29 @@ impl InternalKeyValue {
 
     pub fn from_key(k: String) -> Self {
         Self::from(0, ValueType::KTypeValue, k, String::new())
+    }
+
+    pub fn get_key(&self) -> String {
+        let mut offset = 0;
+        let key_size = utils::decode_varint_32(&self.data[offset..]).unwrap() - 8;
+        offset += utils::varint_length(key_size as u64) as usize;
+        String::from_utf8_lossy(&self.data[offset..offset + key_size as usize]).into_owned()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_key() {
+        let test_key = "test_key".to_string();
+        let kv = InternalKeyValue::from(
+            1,
+            ValueType::KTypeValue,
+            test_key.clone(),
+            "test_value".to_string(),
+        );
+        assert_eq!(kv.get_key(), test_key);
     }
 }
--- src/core/utils.rs
+++ src/core/utils.rs
@@ -32,6 +32,23 @@ pub fn encode_varint_32(dst: &mut [u8], v: u32) -> usize {
     bytes_written
 }
 
+pub fn decode_varint_32(src: &[u8]) -> Option<u32> {
+    const B: u8 = 128;
+    let mut v = 0;
+    let mut shift = 0;
+
+    for &byte in src {
+        if byte < B {
+            v |= (byte as u32) << shift;
+            return Some(v);
+        }
+        v |= ((byte & !B) as u32) << shift;
+        shift += 7;
+    }
+
+    None
+}
+
 pub fn varint_length(mut v: u64) -> u32 {
     let mut len = 1;
     while v >= 128 {
@@ -39,4 +56,40 @@ pub fn varint_length(mut v: u64) -> u32 {
         len += 1;
     }
     len
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encode_and_decode_varint_32() {
+        let mut buf = [0u8; 5];
+        let _ = encode_varint_32(&mut buf, 1);
+        assert_eq!(decode_varint_32(&buf), Some(1));
+
+        let mut buf = [0u8; 5];
+        let _ = encode_varint_32(&mut buf, 3);
+        assert_eq!(decode_varint_32(&buf), Some(3));
+
+        let mut buf = [0u8; 5];
+        let _ = encode_varint_32(&mut buf, 10);
+        assert_eq!(decode_varint_32(&buf), Some(10));
+
+        let mut buf = [0u8; 5];
+        let _ = encode_varint_32(&mut buf, 100);
+        assert_eq!(decode_varint_32(&buf), Some(100));
+
+        let mut buf = [0u8; 5];
+        let _ = encode_varint_32(&mut buf, 1234);
+        assert_eq!(decode_varint_32(&buf), Some(1234));
+
+        let mut buf = [0u8; 5];
+        let _ = encode_varint_32(&mut buf, 123456);
+        assert_eq!(decode_varint_32(&buf), Some(123456));
+
+        let mut buf = [0u8; 5];
+        let _ = encode_varint_32(&mut buf, 3);
+        assert_eq!(decode_varint_32(&buf), Some(3));
+    }
 }
